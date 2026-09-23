280382f37a23a87487398b1164560239a99bc74e
Sathish-0987 <msathishkumar204@gmail.com>
2026-09-23 15:32:50 +0530

    Started by adding name

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Full Stack developer</h1>
+        <p>My name is Sathish Kumar</p>
+    </div>
+  )
+}
