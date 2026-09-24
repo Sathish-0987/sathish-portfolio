4ca9647d142b35c5ec3c458707d327089b369f27
Sathish-0987 <msathishkumar204@gmail.com>
2026-09-24 23:19:45 +0530

    Added the profile Picture

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import profilePicture from './assets/Profile Picture2.jpg'
 import './Home.css'
 
 export default function Home() {
   return (
     <div>
+      <img src={profilePicture} alt='Profile photo' />
         <h1>Full Stack developer</h1>
         <p>My name is Sathish Kumar</p>
         <p>I am BE Computer Science and Engineering Graduate</p>
