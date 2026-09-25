4be1c8c228d1e6afec559239a957daac127f9aef
Sathish-0987 <msathishkumar204@gmail.com>
2026-09-25 14:22:38 +0530

    Added the Self Introduction

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,7 +9,10 @@ export default function Home() {
         <h1>MERN Stack Developer</h1>
         <p>My name is Sathish Kumar</p>
         <p>I am BE Computer Science and Engineering Graduate</p>
-        <p>HI</p>
+        <p>I am a Computer Science and Engineering graduate with a strong interest in web development. I have completed a Full Stack MERN Development course, where I gained hands-on experience with MongoDB, Express.js, React.js, and Node.js.
+
+I enjoy building responsive and user-friendly web applications and continuously improving my development skills. I am currently looking for an opportunity to start my career as a Web Developer or MERN Stack Developer, where I can apply my knowledge, work on real-world projects, and grow as a professional.
+</p>
     </div>
   )
 }
